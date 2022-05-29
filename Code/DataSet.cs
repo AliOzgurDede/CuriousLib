851c5b79eb69409185f5f1dec74e40aa35a21366
Ali Özgür Dede <74831928+AliOzgurDede@users.noreply.github.com>
2022-05-29 13:52:44 +0300

    Update DataSet.cs

--- Code/DataSet.cs
+++ Code/DataSet.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows;
 using System.ComponentModel;
 using System.ComponentModel.Design.Serialization;
 
@@ -124,7 +124,29 @@ namespace CuriousLib
         {
             get
             {
-                return(this.CalculateSkewness());
+                return (this.CalculateSkewness());
+            }
+        }
+
+        /// <summary>
+        /// Gets whether a dataset is normal distributed or not
+        /// </summary>
+        public bool IsNormal
+        {
+            get
+            {
+                return this.TestForNormality();
+            }
+        }
+
+        /// <summary>
+        /// Gets whether a dataset is uniformly distributed or not
+        /// </summary>
+        public bool IsUniform
+        {
+            get
+            {
+                return this.TestForUniformity();
             }
         }
 
@@ -152,7 +174,7 @@ namespace CuriousLib
         public DataSet<double> Smooth(double Alpha)
         {
             double NewItem;
-            DataSet <double> SmoothedDataSet = new DataSet<double>();
+            DataSet<double> SmoothedDataSet = new DataSet<double>();
             for (int i = 0; i < this.Size; i++)
             {
                 if (i == 0)
@@ -222,6 +244,193 @@ namespace CuriousLib
             skewness = sumOfCubes / sumOfSquares;
             skewness = Math.Round(skewness, 2);
             return skewness;
+        }
+
+        /// <summary>
+        /// Conducts hypothesis testing for data normality
+        /// </summary>
+        /// <param name="dataSet"></param>
+        /// <returns>bool</returns>
+        private bool TestForNormality()
+        {
+            // defining test parameters
+            bool Hypothesis;
+            int DegreeOfFreedom = 5;
+            double CriticalValue = ChiSquareTable[DegreeOfFreedom];
+            double ChiSquareValue = 0;
+            // calculating expected frequencies
+            double[] ExpectedFrequency = new double[6];
+            ExpectedFrequency[0] = 2.1 / 100 * this.Size;
+            ExpectedFrequency[1] = 13.6 / 100 * this.Size;
+            ExpectedFrequency[2] = 34.1 / 100 * this.Size;
+            ExpectedFrequency[3] = 34.1 / 100 * this.Size;
+            ExpectedFrequency[4] = 13.6 / 100 * this.Size;
+            ExpectedFrequency[5] = 2.1 / 100 * this.Size;
+            // gathering observed frequencies
+            double[] ObservedFrequency = new double[6];
+            ObservedFrequency[0] = 0;
+            ObservedFrequency[1] = 0;
+            ObservedFrequency[2] = 0;
+            ObservedFrequency[3] = 0;
+            ObservedFrequency[4] = 0;
+            ObservedFrequency[5] = 0;
+
+            for (int i = 0; i < this.Size; i++)
+            {
+                double Z = this.CalculateZvalue(Convert.ToDouble(this[i]));
+
+                if (Z <= -2)
+                {
+                    ObservedFrequency[0] = ObservedFrequency[0] + 1;
+                }
+                else if (Z > -2 && Z <= -1)
+                {
+                    ObservedFrequency[1] = ObservedFrequency[1] + 1;
+                }
+                else if (Z > -1 && Z <= 0)
+                {
+                    ObservedFrequency[2] = ObservedFrequency[2] + 1;
+                }
+                else if (Z > 0 && Z <= 1)
+                {
+                    ObservedFrequency[3] = ObservedFrequency[3] + 1;
+                }
+                else if (Z > 1 && Z <= 2)
+                {
+                    ObservedFrequency[4] = ObservedFrequency[4] + 1;
+                }
+                else if (Z > 2)
+                {
+                    ObservedFrequency[5] = ObservedFrequency[5] + 1;
+                }
+            }
+            // calculating chi square parameter
+            for (int i = 0; i < 6; i++)
+            {
+                ChiSquareValue = ChiSquareValue + Math.Pow(ObservedFrequency[i] - ExpectedFrequency[i], 2) / ExpectedFrequency[i];
+            }
+            // comparing the calculated chi square parameter and critical chi square parameter
+            if (ChiSquareValue <= CriticalValue)
+            {
+                Hypothesis = true;
+            }
+            else
+            {
+                Hypothesis = false;
+            }
+            return Hypothesis;
+        }
+
+        /// <summary>
+        /// Conducts hypothesis testing for data uniformity
+        /// </summary>
+        /// <param name="dataSet"></param>
+        /// <returns>bool</returns>
+        private bool TestForUniformity()
+        {
+            // defining test parameters
+            bool Hypothesis;
+            int DegreeOfFreedom = 5;
+            double CriticalValue = ChiSquareTable[DegreeOfFreedom];
+            double ChiSquareValue = 0;
+            // calculating expected frequencies
+            double[] ExpectedFrequency = new double[6];
+            ExpectedFrequency[0] = 16.6 / 100 * this.Size;
+            ExpectedFrequency[1] = 16.6 / 100 * this.Size;
+            ExpectedFrequency[2] = 16.6 / 100 * this.Size;
+            ExpectedFrequency[3] = 16.6 / 100 * this.Size;
+            ExpectedFrequency[4] = 16.6 / 100 * this.Size;
+            ExpectedFrequency[5] = 16.6 / 100 * this.Size;
+            // gathering observed frequencies
+            double[] ObservedFrequency = new double[6];
+            ObservedFrequency[0] = 0;
+            ObservedFrequency[1] = 0;
+            ObservedFrequency[2] = 0;
+            ObservedFrequency[3] = 0;
+            ObservedFrequency[4] = 0;
+            ObservedFrequency[5] = 0;
+
+            for (int i = 0; i < this.Size; i++)
+            {
+                double Z = this.CalculateZvalue(Convert.ToDouble(this[i]));
+
+                if (Z <= -2)
+                {
+                    ObservedFrequency[0] = ObservedFrequency[0] + 1;
+                }
+                else if (Z > -2 && Z <= -1)
+                {
+                    ObservedFrequency[1] = ObservedFrequency[1] + 1;
+                }
+                else if (Z > -1 && Z <= 0)
+                {
+                    ObservedFrequency[2] = ObservedFrequency[2] + 1;
+                }
+                else if (Z > 0 && Z <= 1)
+                {
+                    ObservedFrequency[3] = ObservedFrequency[3] + 1;
+                }
+                else if (Z > 1 && Z <= 2)
+                {
+                    ObservedFrequency[4] = ObservedFrequency[4] + 1;
+                }
+                else if (Z > 2)
+                {
+                    ObservedFrequency[5] = ObservedFrequency[5] + 1;
+                }
+            }
+            // calculating chi square parameter
+            for (int i = 0; i < 6; i++)
+            {
+                ChiSquareValue = ChiSquareValue + Math.Pow(ObservedFrequency[i] - ExpectedFrequency[i], 2) / ExpectedFrequency[i];
+            }
+            // comparing the calculated chi square parameter and critical chi square parameter
+            if (ChiSquareValue <= CriticalValue)
+            {
+                Hypothesis = true;
+            }
+            else
+            {
+                Hypothesis = false;
+            }
+            return Hypothesis;
+        }
+
+        private Dictionary<double, double> ChiSquareTable = new Dictionary<double, double>();
+        private void GeneratingChiSquareTable()
+        {
+            // key=s.d. value=chisquare
+            // significance degree = %1
+            ChiSquareTable.Add(1, 6.635);
+            ChiSquareTable.Add(2, 9.210);
+            ChiSquareTable.Add(3, 11.345);
+            ChiSquareTable.Add(4, 13.227);
+            ChiSquareTable.Add(5, 15.086);
+            ChiSquareTable.Add(6, 16.812);
+            ChiSquareTable.Add(7, 18.475);
+            ChiSquareTable.Add(8, 20.090);
+            ChiSquareTable.Add(9, 21.666);
+            ChiSquareTable.Add(10, 23.209);
+            ChiSquareTable.Add(11, 24.725);
+            ChiSquareTable.Add(12, 26.217);
+            ChiSquareTable.Add(13, 27.688);
+            ChiSquareTable.Add(14, 29.141);
+            ChiSquareTable.Add(15, 30.578);
+            ChiSquareTable.Add(16, 32.000);
+            ChiSquareTable.Add(17, 33.409);
+            ChiSquareTable.Add(18, 34.805);
+            ChiSquareTable.Add(19, 36.191);
+            ChiSquareTable.Add(20, 37.566);
+            ChiSquareTable.Add(21, 38.932);
+            ChiSquareTable.Add(22, 40.289);
+            ChiSquareTable.Add(23, 41.638);
+            ChiSquareTable.Add(24, 42.980);
+            ChiSquareTable.Add(25, 44.314);
+            ChiSquareTable.Add(26, 45.642);
+            ChiSquareTable.Add(27, 46.963);
+            ChiSquareTable.Add(28, 48.378);
+            ChiSquareTable.Add(29, 49.588);
+            ChiSquareTable.Add(30, 50.892);
         }
 
         #endregion
