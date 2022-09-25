79b1fdca5b49c5e2cdeb7a0034c55fa924e2a5fc
Ali Özgür Dede <74831928+AliOzgurDede@users.noreply.github.com>
2022-09-25 17:37:40 +0300

    Update TrendingDataSet.cs

--- Code/TrendingDataSet.cs
+++ Code/TrendingDataSet.cs
@@ -27,7 +27,7 @@ namespace CuriousLib
         # region Properties
 
         /// <summary>
-        /// Gets the slope of a trending DataSet
+        /// Gets the slope of a TrendingDataSet
         /// </summary>
         /// <remarks> DataSet is required to have trending pattern </remarks>
         public double Slope
@@ -36,7 +36,7 @@ namespace CuriousLib
             {
                 if (this.Pattern == DataSetPattern.Trending)
                 {
-                    return slope = this.GetSlope();
+                    return this.GetSlope();
                 }
                 else
                 {
@@ -44,9 +44,9 @@ namespace CuriousLib
                 }
             }
         }
-        
+
         /// <summary>
-        /// Gets the intercept of a trending DataSet
+        /// Gets the intercept of a TrendingDataSet
         /// </summary>
         /// <remarks> DataSet is required to have trending pattern </remarks>
         public double Intercept
@@ -55,7 +55,7 @@ namespace CuriousLib
             {
                 if (this.Pattern == DataSetPattern.Trending)
                 {
-                    return intercept = this.GetIntercept();
+                    return this.GetIntercept();
                 }
                 else
                 {
@@ -67,14 +67,14 @@ namespace CuriousLib
         #endregion
 
         #region Methods
-        
+
         /// <summary>
         /// Conducts point estimation of independent variable X using linear regression line
         /// </summary>
         /// <remarks> DataSet is required to have trending pattern </remarks>
-        /// <param name="x"></param>
+        /// <param name="X"></param>
         /// <returns> Estimated Value: Y </returns>
-        public double LinearRegression(double X)
+        public double LinearRegression(int X)
         {
             if (this.Pattern == DataSetPattern.Trending)
             {
@@ -88,84 +88,123 @@ namespace CuriousLib
             }
         }
 
+        /// <summary>
+        /// Conducts point estimation of independent variable X using Holt's Method
+        /// </summary>
+        /// <param name="Alpha"></param>
+        /// <param name="Beta"></param>
+        /// <param name="X"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public double HoltsMethod(double Alpha, double Beta, int X)
+        {
+            if (this.Pattern == DataSet<T>.DataSetPattern.Trending)
+            {
+                if (Alpha > 0 && Alpha < 1 && Beta > 0 && Beta < 1)
+                {
+                    double Forecast = 0;
+                    // Setup for smoothed intercepts
+                    DataSet<double> SmoothedIntercepts = new DataSet<double>();
+                    double InitialSmoothedIntercept = Convert.ToDouble(this[0]);
+                    SmoothedIntercepts.Add(InitialSmoothedIntercept);
+                    double CurrentSmoothedIntercept;
+                    // Setup for smoothed slopes
+                    DataSet<double> SmoothedSlopes = new DataSet<double>();
+                    double InitialSmoothedSlope = Convert.ToDouble(this[1]) - Convert.ToDouble(this[0]);
+                    SmoothedSlopes.Add(InitialSmoothedSlope);
+                    double CurrentSmoothedSlope;
+
+                    for (int i = 1; i < this.Size; i++)
+                    {
+                        CurrentSmoothedIntercept = Alpha * Convert.ToDouble(this[i]) + ((1 - Alpha) * (SmoothedIntercepts[i - 1] + SmoothedSlopes[i - 1]));
+                        SmoothedIntercepts.Add(CurrentSmoothedIntercept);
+                        CurrentSmoothedSlope = Beta * (SmoothedIntercepts[i] - SmoothedIntercepts[i - 1]) + ((1 - Beta) * SmoothedSlopes[i - 1]);
+                        SmoothedSlopes.Add(CurrentSmoothedSlope);
+
+                        if (X <= this.Size)
+                        {
+                            if (i == X - 1)
+                            {
+                                Forecast = CurrentSmoothedIntercept + CurrentSmoothedSlope;
+                                break;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            if (i == this.Size - 1)
+                            {
+                                Forecast = CurrentSmoothedIntercept + ((X - this.Size) * CurrentSmoothedSlope);
+                            }
+                        }
+                    }
+
+                    return Forecast;
+                }
+                else
+                {
+                    throw new Exception("Invalid Alpha or Beta value");
+                }
+            }
+            else
+            {
+                throw new Exception("DataSet pattern is not valid. Trending pattern expected");
+            }
+        }
+
         #endregion
 
         #region Privates
 
-        private double slope;
-        private double intercept;
-        private int[] TimeAxis;
+        /// <summary>
+        /// Calculates the slope of a TrendingDataSet
+        /// </summary>
+        /// <returns>Double</returns>
         private double GetSlope()
         {
-            double _slope;
-            int N = this.Count;
-            TimeAxis = new int[N];
-            for (int i = 0; i < N; i++)
+            double slope = 0;
+            DataSet<double> TimeAxis = new DataSet<double>();
+            DataSet<double> OriginalDataSet = new DataSet<double>();
+
+            for (int i = 0; i < this.Size; i++)
             {
-                TimeAxis[i] = i + 1;
+                TimeAxis.Add(i + 1);
             }
 
-            double xBar = 0;
-            double ttl = 0;
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < this.Size; i++)
             {
-                ttl = ttl + TimeAxis[i];
-            }
-            xBar = ttl / N;
-            xBar = Math.Round(xBar, 2);
-            double yBar = 0;
-            double Total = 0;
-            for (int i = 0; i < N; i++)
-            {
-                Total = Total + Convert.ToDouble(this[i]);
-            }
-            yBar = Total / N;
-            yBar = Math.Round(yBar, 2);
-
-            double sumOfXcrossY = 0;
-            for (int i = 0; i < N; i++)
-            {
-                sumOfXcrossY = sumOfXcrossY + (Convert.ToDouble(this[i]) * TimeAxis[i]);
+                OriginalDataSet.Add(Convert.ToDouble(this[i]));
             }
 
-            double sumOfXsquare = 0;
-            for (int i = 0; i < N; i++)
-            {
-                sumOfXsquare = sumOfXsquare + (Math.Pow(TimeAxis[i], 2));
-            }
-
-            _slope = (sumOfXcrossY - (N * xBar * yBar)) / (sumOfXsquare - (N * xBar * xBar));
-            return _slope;
+            slope = Miscellaneous.Correlation(OriginalDataSet, TimeAxis) * OriginalDataSet.StandartDeviation / TimeAxis.StandartDeviation;
+            return slope;
         }
+
+        /// <summary>
+        /// Calculates the intercept of a TrendingDataSet
+        /// </summary>
+        /// <returns>Double</returns>
         private double GetIntercept()
         {
-            double _intercept;
-            int N = this.Count;
-            TimeAxis = new int[N];
-            for (int i = 0; i < N; i++)
+            double intercept;
+            DataSet<double> TimeAxis = new DataSet<double>();
+            DataSet<double> OriginalDataSet = new DataSet<double>();
+
+            for (int i = 0; i < this.Size; i++)
             {
-                TimeAxis[i] = i + 1;
+                TimeAxis.Add(i + 1);
             }
 
-            double xBar = 0;
-            double ttl = 0;
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < this.Size; i++)
             {
-                ttl = ttl + TimeAxis[i];
+                OriginalDataSet.Add(Convert.ToDouble(this[i]));
             }
-            xBar = ttl / N;
-            xBar = Math.Round(xBar, 2);
-            double yBar = 0;
-            double Total = 0;
-            for (int i = 0; i < N; i++)
-            {
-                Total = Total + Convert.ToDouble(this[i]);
-            }
-            yBar = Total / N;
-            yBar = Math.Round(yBar, 2);
 
-            _intercept = yBar - (this.Slope * xBar);
-            return _intercept;
+            intercept = OriginalDataSet.Mean - (this.Slope * TimeAxis.Mean);
+            return intercept;
         }
 
         #endregion
