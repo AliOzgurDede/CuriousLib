3181f0af3865d3faaa969be6d1480c466c43f058
Ali Özgür Dede <74831928+AliOzgurDede@users.noreply.github.com>
2022-10-23 13:08:47 +0300

    Create ClusterCenter.cs

---
+++ Code/Clustering/ClusterCenter.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuriousLib.Clustering
+{
+    public class ClusterCenter
+    {
+        /// <summary>
+        /// Gets or sets the ID of a ClusterCenter
+        /// </summary>
+        public int ID
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets ot sets the X coordinate of a ClusterCenter
+        /// </summary>
+        public double CoordinateX
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Y coordinate of a ClusterCenter
+        /// </summary>
+        public double CoordinateY
+        {
+            get; set;
+        }
+    }
+}
